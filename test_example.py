90fd61a8c4cdf4999b8c1c3f01457de6324a4932
Daafip <daafips@gmail.com>
2024-02-29 12:13:33 +0100

    another failing test

--- test_example.py
+++ test_example.py
@@ -3,5 +3,8 @@ from example import add, subtract
 def test_add():
     assert add(1,1) == 2
     
+def test_add_str():
+    assert add("hello","world") == "hello world"
+    
 def test_subtract():
     assert subtract(2,1) == 1
